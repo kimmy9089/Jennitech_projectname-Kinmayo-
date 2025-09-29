1113f500a028c02a8292e28aafa4737ad4a89a6c
kimmy9089 <kinmayo27@gmail.com>
2025-09-29 04:45:28 +0100

    Add files via upload

---
+++ Multiplication.js
@@ -0,0 +1,12 @@
+// Program to print multiplication table of 2
+
+function multiplicationTableOfTwo() {
+    console.log("Multiplication Table of 2");
+    console.log("--------------------------");
+    for (let i = 1; i <= 12; i++) {
+        console.log(`2 x ${i} = ${2 * i}`);
+    }
+}
+
+// Call the function
+multiplicationTableOfTwo();
